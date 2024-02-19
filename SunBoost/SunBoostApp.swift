2330fcdea8e0941727c7836748149119e75f2da5
Mohit <92150824+MKG26@users.noreply.github.com>
2024-02-19 11:56:42 +0530

    Initial Commit

---
+++ SunBoost/SunBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunBoostApp.swift
+//  SunBoost
+//
+//  Created by Mohit Kumar Gupta on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SunBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
