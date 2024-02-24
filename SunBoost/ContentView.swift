c20e22cfcb1d0ac4db7eccaddf6467681bb5a91d
Mohit <92150824+MKG26@users.noreply.github.com>
2024-02-24 14:02:30 +0530

    Update ContentView.swift

--- SunBoost/ContentView.swift
+++ SunBoost/ContentView.swift
@@ -8,16 +8,125 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingSecondView = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        
+            
+                
+            NavigationView{
+                
+                        
+                        ZStack{
+                        
+                            
+
+                            
+                            VStack {
+                                
+                                
+                                
+                                Text("Do You Know?")
+                                    .font(.system(size: 30, weight: .bold))
+                                    .foregroundColor(.blue)
+                                    .fontWeight(.heavy)
+                                    
+                                    
+                                    .padding(.init(top: 220, leading: 10, bottom: 15, trailing: 0))
+                                
+                                
+                                
+                                
+                                Text("About 1 billion worldwide have vitamin D deficiency or levels.")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 30, weight: .bold))
+                                    .multilineTextAlignment(.center)
+                                    .padding(.init(top: 30, leading: 5, bottom: 20, trailing: 5))
+                                    
+                                
+                                
+                                
+                                
+                                
+                                Spacer()
+                                
+                                    
+                                
+                                
+                                NavigationLink( destination: SecondPage(), isActive: $showingSecondView){
+                                    Button{
+                                        
+                                        showingSecondView = true
+                                        
+                                        
+                                    }label: {
+                                        
+                                        
+                                        
+                                        Text("Next")
+                                            .font(.system(size: 30, weight: .bold))
+                                            .foregroundStyle(LinearGradient (colors: [.blue, .pink],
+                                                                             startPoint: .leading, endPoint: .trailing))
+                                            
+                                            
+                                        
+                                            
+                                        
+                                            .frame(width: 300,height: 60)
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                        
+                                            
+                                            .background (
+                                                    ZStack {
+                                                        Color(Color.blue.opacity(0.2))
+                                                        RoundedRectangle (cornerRadius: 16, style:
+                                                                .continuous)
+                                                                .foregroundColor(.white)
+                                                                .blur(radius: 4)
+                                                        }
+                                                    )
+                                                    .clipShape (RoundedRectangle (cornerRadius: 16, style:.continuous))
+                                                    .shadow(color: Color(Color.gray.opacity(0.1)),radius: 20, x: 20, y: 20)
+                                                    .shadow(color: Color( Color.gray.opacity(0.1)),radius: 20, x: -20, y: -20)
+                                                    
+                                                    .padding(.init(top: 0, leading: 0, bottom: 220, trailing: 0))
+                                            
+                                    }
+                                            .background (Color(Color.white))
+                                            .edgesIgnoringSafeArea(.all)
+                                    
+                                    
+                                            
+                                }
+                                
+                                
+                                
+                                
+                                
+                            }
+                            
+                        }
+                        }
         }
-        .padding()
-    }
-}
+            }
+        
+        
+        
+        
+    
+
+    
+
+
+
 
 #Preview {
     ContentView()
