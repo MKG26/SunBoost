711d6b0643a0d7d807eafcff3a5491b61b699408
Mohit <92150824+MKG26@users.noreply.github.com>
2024-02-24 14:02:54 +0530

    Create Navigate.swift

---
+++ SunBoost/Navigate.swift
@@ -0,0 +1,21 @@
+//
+//  Navigate.swift
+//  SunBoost
+//
+//  Created by Mohit Kumar Gupta on 20/02/24.
+//
+
+import SwiftUI
+
+struct Navigate: View {
+    var body: some View {
+        NavigationStack{
+            
+            ContentView()
+        }
+    }
+}
+
+#Preview {
+    Navigate()
+}
